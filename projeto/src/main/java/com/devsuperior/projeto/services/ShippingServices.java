d49d6c2daadd307d15af59fa396819ec5a3af223
William Conan Mateus <williamcsouza2022@gmail.com>
2024-02-10 20:08:07 -0300

    ShippingServices OrderService

--- projeto/src/main/java/com/devsuperior/projeto/services/ShippingServices.java
+++ projeto/src/main/java/com/devsuperior/projeto/services/ShippingServices.java
@@ -6,14 +6,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShippingServices {
 
-
     public double shipment(Order order) {
 
         if (order.getBasic() < 100) {
             return order.getBasic() + 20;
         }
 
-        if (order.getBasic() >= 100 || order.getBasic() <= 200) {
+        if (order.getBasic() >= 100 && order.getBasic() <= 200) {
 
             return order.getBasic() + 12;
         }
